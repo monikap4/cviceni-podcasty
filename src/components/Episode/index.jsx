163197c24f61ef289cbb3e5cad05bf65b301f0cc
Monika Pazderová <monika.pazderova@igalileo.cz>
2025-06-01 15:46:20 +0200

    pridani seznamu

---
+++ src/components/Episode/index.jsx
@@ -0,0 +1,17 @@
+import './index.css'
+
+export const Episode = ({ num, title, guest }) => {
+    return (
+        <>
+            <div className="episodes-list">
+                <div className="episode">
+                    <div className="episode__num">{num}</div>
+                    <div className="episode__body">
+                        <div className="episode__title">{title}</div>
+                        <div className="episode__guest">{guest}</div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
